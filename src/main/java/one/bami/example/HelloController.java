df3b3138a8ec4b493f683d3f537fe48dcf57da39
sseoseungbeom <seungbami21@gmail.com>
2025-07-08 05:51:58 +0000

    v2.2.0

--- src/main/java/one/bami/example/HelloController.java
+++ src/main/java/one/bami/example/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/test")
     public String hello() {
-        return "Hello World010a";
+        return "Hello World010aA";
     }
 
     @GetMapping("/test2")
